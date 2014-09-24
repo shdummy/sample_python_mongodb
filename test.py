686679fd23bb5f17682a07cdeea753c607737852
Crash <a7335788@drdrb.net>
2014-09-24 13:11:28 -0700

    Update test.py

--- test.py
+++ test.py
@@ -16,21 +16,6 @@ class SomeTests(unittest.TestCase):
         mongo.populate()
         things = mongo.count()
         self.failIf(things != 5)
-        
-class OtherTests(unittest.TestCase):
-    
-    def test_passing(self):
-        p = True
-        self.failIf(p != True)
-        
-    def test_passing_too(self):
-        p = True
-        self.failIf(p != True)
-        
-    def test_passing_three(self):
-        p = True
-        self.failIf(p != True)
-
 
 def main():
     unittest.main()
