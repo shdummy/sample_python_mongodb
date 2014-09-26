1085d9921fb38a47a66d95ece48234d11bcf4228
Crash <a7335788@drdrb.net>
2014-09-26 10:13:55 -0700

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -18,7 +18,7 @@ class Mongo():
         self.db.things.insert(things)
 
     def count(self):
-        raise Exception("There was an error in the program")
+        raise Exception("There was an error in the program and it has a really long error message seriously it's so long it's like it's never going to end it just keeps going and going and going an going and going and going and going and going")
         return self.db.things.count()
 
 if __name__ == "__main__":
