d33fe813f399d3a4c8a5e3aef1d7db4628701f14
Crash <a7335788@drdrb.net>
2014-09-26 10:15:26 -0700

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -18,7 +18,14 @@ class Mongo():
         self.db.things.insert(things)
 
     def count(self):
-        raise Exception("There was an error in the program and it has a really long error message seriously it's so long it's like it's never going to end it just keeps going and going and going an going and going and going and going and going")
+        raise Exception('''Traceback (most recent call last):
+  File "/usr/lib/python2.7/unittest/case.py", line 327, in run
+    testMethod()
+  File "/home/shippable/workspace/src/github.com/shdummy/sample_python_mongodb/test.py", line 17, in test
+    things = mongo.count()
+  File "/home/shippable/workspace/src/github.com/shdummy/sample_python_mongodb/mongo.py", line 21, in count
+    raise Exception("There was an error in the program and it has a really long error message seriously it's so long it's like it's never going to end it just keeps going and going and going an going and going and going and going and going")
+Exception: There was an error in the program and it has a really long error message seriously it's so long it's like it's never going to end it just keeps going and going and going an going and going and going and going and going''')
         return self.db.things.count()
 
 if __name__ == "__main__":
